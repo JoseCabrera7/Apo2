1650a72856a2ad53508edc997c0da4e1f3da81ba
Juan Jaramillo <jufejaramillo123@umariana.edu.co>
2023-10-17 21:01:58 -0500

    Arregle unos errores del sistema de promedias

--- promedio.java
+++ promedio.java
@@ -5,13 +5,13 @@ public class promedio {
     public class Promedio
     {
         public static void main(String[] args) {
-        Scanner ss=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
             System.out.print("Ingrese Calificaciones");
             float n1 = sc.nextFloat ();
             float n2 = sc.nextFloat();
             float n3 = sc.nextFloat();
             float prom = (n1 + n2 + n3)/3;
-        System. out. print(prom);
-    }
+        System.out.print(prom);
+        }
     }   
 }
