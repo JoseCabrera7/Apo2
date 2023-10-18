449945962516def79d4955c6ed1724feb2826dbc
Jose Cabrera <josecab1948@gmail.com>
2023-10-17 21:21:40 -0500

    agrego codigo de multiplicacion

--- multiplicacion.java
+++ multiplicacion.java
@@ -4,12 +4,10 @@ public class multiplicacion {{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese el primer número: ");
-        int num1 = sc.nextInt();
+            int numero1 = sc.nextInt();
         System.out.print("Ingrese el segundo número: ");
-        int num2 = sc.nextInt();
-        int resultado = num1 * num2;
+            int numero2 = sc.nextInt();
+            int resultado = numero1 * numero2;
         System.out.println("El resultado de la multiplicación es: " + resultado);
-    }
 }
-
 }
