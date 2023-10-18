4bb1e354ec250c8adc9053c31255e4105f4b5f9b
Juan Jaramillo <jufejaramillo123@umariana.edu.co>
2023-10-17 20:59:53 -0500

    Agregue un sistema que me promedio notas

---
+++ promedio.java
@@ -0,0 +1,17 @@
+package Apo2;
+
+import java.util.Scanner;
+public class promedio {
+    public class Promedio
+    {
+        public static void main(String[] args) {
+        Scanner ss=new Scanner(System.in);
+            System.out.print("Ingrese Calificaciones");
+            float n1 = sc.nextFloat ();
+            float n2 = sc.nextFloat();
+            float n3 = sc.nextFloat();
+            float prom = (n1 + n2 + n3)/3;
+        System. out. print(prom);
+    }
+    }   
+}
