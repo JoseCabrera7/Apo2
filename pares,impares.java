24eba4c926ebeb090938c553525caecf7162824f
Juan Jaramillo <jufejaramillo123@umariana.edu.co>
2023-10-17 20:44:05 -0500

    Corregi par e impar

--- pares,impares.java
+++ pares,impares.java
@@ -1,7 +1,7 @@
 package Apo2;
-
+import java.util.Scanner;
 public class pares {
-    import java.util.Scanner;
+    
 	public static void main(String[] args) {
 	try (Scanner sc = new Scanner(System.in)) {
 		int n=sc.nextInt();
